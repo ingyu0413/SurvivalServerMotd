60b712a5a54ddfd719c3ec1e14a016134f6f2b2f
ingyu0413 <ingyu0413@naver.com>
2021-05-26 17:38:45 +0900

    Better readabilities

--- src/main/java/com/github/ingyu0413/survivalServerMotd/SurvivalServer.java
+++ src/main/java/com/github/ingyu0413/survivalServerMotd/SurvivalServer.java
@@ -13,12 +13,17 @@ public final class SurvivalServer extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
+    private String getworldtime() {
+        long gametime = Bukkit.getServer().getWorld("world").getTime() + 6000;
+        int hour = (int) gametime / 1000;
+        int minute = (int) (gametime % 1000 / 1000.0 * 60);
+        int second = (int) (gametime % 1000 / 1000.0 * 3600 % 60);
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
     @EventHandler
     public void onPaperServerListPing(PaperServerListPingEvent e) {
-        // e.setNumPlayers(1);
-        long gametime = Bukkit.getServer().getWorld("world").getTime() + 6000;
-        String time = String.format("%02d:%02d:%02d", gametime / 1000, (int) (gametime % 1000 / 1000.0 * 60), (int) (gametime % 1000 / 1000.0 * 3));
-        e.setMotd(ChatColor.BLUE + "" + ChatColor.BOLD + "키뮤의 아틀리에 비공식 마크서버" + ChatColor.RESET + " " + ChatColor.DARK_GRAY + time);
+        e.setMotd(ChatColor.BLUE + "" + ChatColor.BOLD + "키뮤의 아틀리에 비공식 마크서버" + ChatColor.RESET + " " + ChatColor.DARK_GRAY + getworldtime());
         e.getPlayerSample().clear();
     }
 }
