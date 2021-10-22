54775c7431c9345a3d1d7536a610c4fd95475694
ingyu0413 <ingyu0413@naver.com>
2021-10-22 23:45:31 +0900

    TIME COLOR ADDED

--- src/main/java/com/github/ingyu0413/survivalServerMotd/SurvivalServer.java
+++ src/main/java/com/github/ingyu0413/survivalServerMotd/SurvivalServer.java
@@ -13,13 +13,17 @@ public final class SurvivalServer extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    private String getworldtime() {
+    private long getWorldTime() {
         long gametime = Bukkit.getServer().getWorld("world").getTime();
-        if (gametime <= 18000) {
-            gametime += 6000;
-        } else if (gametime > 18000) {
-            gametime -= 18000;
-        }
+        return (gametime + 6000) % 24000;
+    }
+
+    private int getHour() {
+        return (int) getWorldTime() / 1000;
+    }
+
+    private String getWorldTimeString() {
+        long gametime = getWorldTime();
         int hour = (int) gametime / 1000;
         int minute = (int) (gametime % 1000 / 1000.0 * 60);
         int second = (int) (gametime % 1000 / 1000.0 * 3600 % 60);
@@ -28,7 +32,18 @@ public final class SurvivalServer extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPaperServerListPing(PaperServerListPingEvent e) {
-        e.setMotd(ChatColor.BLUE + "" + ChatColor.BOLD + "24시간 생야생 마크서버" + ChatColor.RESET + " " + ChatColor.DARK_GRAY + getworldtime());
+        ChatColor titlecolor = null;
+        int hour = getHour();
+        if (hour < 5 || hour >= 19) {
+            titlecolor = ChatColor.DARK_BLUE; //night
+        } else if (hour < 7) {
+            titlecolor = ChatColor.BLUE; //sunrise
+        } else if (hour < 18) {
+            titlecolor = ChatColor.AQUA; //day
+        } else {
+            titlecolor = ChatColor.GOLD; //sunset
+        }
+        e.setMotd(titlecolor + "" + ChatColor.BOLD + "마인크래프트 야생서버" + ChatColor.RESET + " " + ChatColor.DARK_GRAY + "time: " + getWorldTimeString());
         e.getPlayerSample().clear();
     }
 }
